21c7961dc3e8156c7410d7a7bd7e4354188c900c
Ationic <igargon481@g.educaand.es>
2024-06-06 21:44:37 +0200

    added also a bit doc to the main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//! Este módulo principal configura y ejecuta la ventana principal del juego.
+//! 
+//! El juego utiliza la biblioteca `piston_window` para manejar la ventana y
+//! los eventos del juego. La lógica del juego se organiza a través de
+//! diferentes módulos y el `ScreenManager` se encarga de gestionar las
+//! diferentes pantallas del juego.
 mod player;
 mod chars;
 mod views;
